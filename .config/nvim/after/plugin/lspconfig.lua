2f948b958d1ebc3a94fd7ba1ec4bb9922ba8a7f9
lecterkn <lecterkn@proton.me>
2025-01-06 01:33:27 +0900

    [neovim] add ensure installed lsp

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "rust_analyzer", "gopls" },
+    ensure_installed = { "html", "csslsp", "ts_ls", "lua_ls", "rust_analyzer", "gopls", "zls" },
 })
 
 local lspconfig = require("lspconfig")

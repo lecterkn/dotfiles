195900ebc9e00ec6d5abd36d8c8075dcb5b622a8
lecterkn <lecterkn@proton.me>
2025-01-14 09:48:02 +0900

    [neovim] fixed theme settings

--- .config/nvim/after/plugin/themes.lua
+++ .config/nvim/after/plugin/themes.lua
@@ -36,10 +36,6 @@ require("catppuccin").setup({
     no_italic = true,
 })
 
-require("vague").setup({
-    transparent = true, -- don't set background
-})
-
 -- nord
 vim.g.nord_disable_background = true
 vim.g.nord_italic = false

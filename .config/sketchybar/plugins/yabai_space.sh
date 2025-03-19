3156cce30c5ef3239d7d9287e4838abe930b561a
lecterkn <lecterkn@proton.me>
2025-03-20 02:01:23 +0900

    [sketchybar] fix selected space

--- .config/sketchybar/plugins/yabai_space.sh
+++ .config/sketchybar/plugins/yabai_space.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# 現在のスペースを取得
-CURRENT_SPACE=$(yabai -m query --spaces | jq '.[] | select(.focused==1).index')
-
-# 全スペースの状態を更新
-for i in {1..5}; do
-    if [[ $i -eq $CURRENT_SPACE ]]; then
-        sketchybar --set space.$i background.drawing="$SELECTED"
-    else
-        sketchybar --set space.$i
-    fi
-done
-
+sketchybar --set "$NAME" background.drawing="$SELECTED"
